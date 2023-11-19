b200e586c4fa660f50bc49409919f17431966042
omoruyi <osakueomoruyi@gmail.com>
2023-11-19 18:10:24 +0100

    backend task

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
+
 const express = require("express");
 const cors = require("cors");
-//const dbConfig = require("./db");
+const dbConfig = require("./db");
 const {envVariable} = require("./utils");
-
-console.log(envVariable)
 
 const app = express();
 app.use(cors()).use(express.json());
 
-  // api
-// app.use("/api", api);
 
   // Catch 404 routes
 app.all("*", (req, res, next) => {
@@ -20,13 +17,11 @@ app.all("*", (req, res, next) => {
 })
 
 
-(async () => {
-  try {
-   // await dbConfig();
-    app.listen(envVariable.PORT, () => {
-      console.log(`Server listening on ${envVariable.PORT}`);
-    });
-  } catch (error) {
-    console.log(error.message);
-  }
-})();
+async function startServer() {
+  await dbConfig()
+  app.listen(envVariable.PORT, () => {
+    console.log(`Server listening on ${envVariable.PORT}`);
+  });
+}
+
+startServer()
